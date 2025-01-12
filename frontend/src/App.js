32f5511fddf88021376953bc01315ea936c32aaf
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-01-12 22:48:39 +0530

    2 elements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import AnimatedComponent from "./Components/AnimatedComponent";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Headlines from "./Components/Headlines";
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<AnimatedComponent />} />
+          <Route path="/countries" element={< />} />
+          <Route path="/headlines" element={<Headlines />} />
         </Routes>
       </Router>
     </div>
